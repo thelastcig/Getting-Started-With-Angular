3da69cd6ab5bfb7697cee48bfe938801ef2b75dc
thelastcig <sonusharmaworkspace@gmail.com>
2025-03-05 22:53:10 +0530

    [Sonu Sharma] Add. Display Hello from BridgeLabz with a user input name.

--- hello-world-app/src/app/app.component.ts
+++ hello-world-app/src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: [FormsModule]
 })
 export class AppComponent {
-  logoUrl: string = 'BL_logo_square_png.png';
-
-  openBridgeLabz(){
-    window.open('https://www.bridgelabz.com', '_blank');
-  }
+  userName: string = '';
 }
