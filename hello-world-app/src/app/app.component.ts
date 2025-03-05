bbca1671218ba33248689cf6333c37be6563d945
thelastcig <sonusharmaworkspace@gmail.com>
2025-03-05 22:21:23 +0530

    [Sonu Sharma] Add. Display BridgeLabz Logo using Property Binding Technique

--- hello-world-app/src/app/app.component.ts
+++ hello-world-app/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  message: string = 'Hello from BridgeLabz!';
+  logoUrl: string = 'BL_logo_square_png.png';
 }
