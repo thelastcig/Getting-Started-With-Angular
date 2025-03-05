302f8b6bcb2e638d97b311e3cea004e50ea852d5
thelastcig <sonusharmaworkspace@gmail.com>
2025-03-05 23:09:10 +0530

    [Sonu Sharma] Add. Display Hello with user name and check for user name validation.

--- hello-world-app/src/app/app.component.ts
+++ hello-world-app/src/app/app.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [FormsModule]
+  imports: [FormsModule, CommonModule]
 })
 export class AppComponent {
   userName: string = '';
+  errorMessage: string = '';
+
+
+  validateUserName(): void {
+    const namePattern = /^[A-Z][a-zA-Z]{2,}$/;
+    if(!namePattern.test(this.userName)){
+      this.errorMessage = 'Name must start with a capital letter and have at least 3 letters.';
+    }else {
+      this.errorMessage = '';
+    }
+  }
 }
